71ad69a76d5d50318038d4f934020e16c866bc5a
Loredana <zaharialoredana43@gmail.com>
2020-03-22 14:33:30 +0200

    Am adaugat constructorul

---
+++ Angajat.java
@@ -0,0 +1,19 @@
+package ro.ase.csie.cts.grupa1085.git.tema2;
+
+public class Angajat {
+String nume;
+String prenume;
+String ocupatie;
+int[] proiecte;
+float salariul;
+public Angajat(String nume, String prenume, String ocupatie, int[] proiecte, float salariul) {
+	super();
+	this.nume = nume;
+	this.prenume = prenume;
+	this.ocupatie = ocupatie;
+	this.proiecte = proiecte;
+	this.salariul = salariul;
+}
+
+
+}
