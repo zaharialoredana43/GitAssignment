dd8f17d6ecb7bfed0b375b551b4047932d181fe0
Loredana <zaharialoredana43@gmail.com>
2020-03-22 14:40:49 +0200

    Am adaugat interfata

---
+++ INumarProiecte.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.grupa1085.git.tema2;
+
+public interface INumarProiecte {
+public abstract int calculeazaNumarulProiectelor();
+public abstract boolean verificaProductivitatea();
+}
