c516f99448d57bebecbe0ce49ae240b81a78246c
Loredana <zaharialoredana43@gmail.com>
2020-03-22 14:29:44 +0200

    TestGit

---
+++ TestGit.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.grupa1085.git.tema2;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este: Depozite de date cu aplicatie intr-o societate comerciala");
+
+	}
+
+}
